8f2621dfd4e86edd5e0a5d6000c3d3b78032dee6
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-04-11 00:12:19 +0600

    login part done

---
+++ src/components/Signup/SignUp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SignUp.css'
+
+const SignUp = () => {
+    return (
+        <div>
+            <p>This is  Sign up</p>
+        </div>
+    );
+};
+
+export default SignUp;
